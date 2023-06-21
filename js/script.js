81736e2635a45929a608c8d3d09489e1fe5a1478
Rakib-siddiki <rmrakib901@gmail.comgit config --global user.name Rakib-siddikiglobal user.email rmrakib901@gmail.comgit config --global user.email rmrakib901@gmail.com>
2023-06-21 13:04:02 +0600

    update colors

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let age = document.querySelector (".age")
+let check = document.querySelector ("button")
+let output = document.querySelector("h1")
+// =============
+let job = document.querySelector(" .job")
+// ==============
+let salary = document.querySelector(".salary")
+
+check.addEventListener ("click",function () {
+   if(age.value >= 18 && age.value < 40 ){
+    if (job.value.toLowerCase() === "yes") {
+       if (salary.value >= 20000) {
+         output.innerHTML = "Selected 😊";
+         output.style.color= "#00ff38"
+       } else {
+         output.innerHTML = "Rejected 😔";
+         output.style.color= "red"
+       }
+    } else {
+      output.innerHTML = "find a job first😒"
+      output.style.color= "red"
+    }
+   }else if (age.value >=40) {
+      output.innerHTML = " You're too old🤦‍♂️"
+      output.style.color = "white"
+   } 
+   else if(age.value == ''){
+      output.innerHTML = "INVALID";
+      output.style.color= "red"
+   }
+   else{
+      output.innerHTML = "Get outta here😒";
+      output.style.color= "red"
+   }
+   
+   
+   
+})
